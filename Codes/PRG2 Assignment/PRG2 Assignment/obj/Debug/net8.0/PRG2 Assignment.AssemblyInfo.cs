5db3bd4a02921865fd7040fc9d9e2e8f506b07de
Morgen Yap <morgenyap@gmail.com>
2025-01-16 14:02:43 +0800

    Creation of Terminal and BoardingGate classes
    Empty classes with no code. To work on it later today

--- Codes/PRG2 Assignment/PRG2 Assignment/obj/Debug/net8.0/PRG2 Assignment.AssemblyInfo.cs
+++ Codes/PRG2 Assignment/PRG2 Assignment/obj/Debug/net8.0/PRG2 Assignment.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PRG2 Assignment")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+50a615d4c5c4c02e2eea7a9488a69bd48b5dfbb1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59c987c82470ef84768371bbdb5a6298973bf2a2")]
 [assembly: System.Reflection.AssemblyProductAttribute("PRG2 Assignment")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PRG2 Assignment")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
